64b60f10dd5e4910fa5929cee7d1e608bb40eab9
jonfowler <jfowler27@gmail.com>
2018-12-09 06:24:37 -0700

    Add text input functionality
    Added text input functionality to the main body of the body.

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,35 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext build) {
     return new Scaffold(
       appBar: AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(
+            hintText: "Send a message"
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
