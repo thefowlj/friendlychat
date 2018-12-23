57265946f66f067b2df52c7f927890d569a2a1e9
jonfowler <jfowler27@gmail.com>
2018-12-23 11:20:27 -0700

    Update message interface dynamically updates
    The interface for sending a message is updated to dynamically change the send button icon color/state based on if text has been entered into the composition text field. When no new text has been added to the composition text field, the message will no longer be "sent" until text is added.

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext build) {
@@ -60,6 +61,11 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
             new Flexible(
               child: new TextField(
                 controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
                 onSubmitted: _handleSubmitted,
                 decoration: new InputDecoration.collapsed(
                     hintText: "Send a message"
@@ -70,7 +76,9 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
               child: new IconButton(
                   icon: new Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)
+                  onPressed: _isComposing
+                    ? () => _handleSubmitted(_textController.text)
+                    : null,
               ),
             ),
           ],
@@ -81,6 +89,9 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
